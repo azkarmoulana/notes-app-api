2c891a8cd18b5bd2de104464935f1b1d5236ec37
azkarmoulana <azkar.moulana@gmail.com>
2020-05-18 00:05:28 +0530

    created delete notes handler

---
+++ handlers/delete.js
@@ -0,0 +1,15 @@
+import handler from "../libs/handler-lib";
+import dynamoDb from "../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+
+    await dynamoDb.delete(params);
+    return { status: true };
+});
