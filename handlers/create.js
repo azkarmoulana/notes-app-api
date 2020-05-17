b30206f82db95dcecc85953e94debd1b484dd12b
azkarmoulana <azkar.moulana@gmail.com>
2020-05-17 22:28:23 +0530

    refactor -> create handler

--- handlers/create.js
+++ handlers/create.js
@@ -1,45 +1,21 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "../libs/handler-lib";
+import dynamoDb from "../libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
 
-export function main(event, context, callback) {
-    const data = JSON.parse(event.body);
+  await dynamoDb.put(params);
+  return params.Item;
 
-    const params = {
-        TableName: process.env.tableName,
-        Item: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            noteId: uuid.v1(),
-            content: data.content,
-            attachment: data.attachment,
-            createdAt: Date.now()
-        }
-    };
-
-    dynamoDb.put(params, (error, data) => {
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers,
-                body: JSON.stringify({ status: false })
-            };
-
-            callback(null, response);
-            return;
-        }
-
-        const response = {
-            statusCode: 200,
-            headers,
-            body: JSON.stringify(params.Item)
-        };
-
-        callback(null, response);
-    });
-}
+});
